6678370075c3b67a0abeaa9b99be3ed558825463
Lucifer1662 <l.hawk99@hotmail.com>
2022-08-31 12:19:26 +1000

    widen img

--- gen_table.py
+++ gen_table.py
@@ -9,7 +9,7 @@ result = [os.path.join(dp, f) for dp, dn, filenames in os.walk(path) for f in fi
 def convert(path: str):
     className = path[path.rfind("\\")+1:].replace(".png","")
     imgPath = "https://raw.githubusercontent.com/Digital-Dance-TrakLab/Traklab-Documentation/master/" + path.replace("\\","/")   
-    markdownImg = "<img width=400 src=\"" + imgPath + "\" />"
+    markdownImg = "<img width=800 src=\"" + imgPath + "\" />"
     return (className, markdownImg)
     
 
